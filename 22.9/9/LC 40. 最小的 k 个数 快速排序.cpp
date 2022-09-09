fe38d61d726f9a1717dcf7e0aaec6493d8ff1cd9
Vinter8848 <105093677+Vinter8848@users.noreply.github.com>
2022-09-09 23:52:48 +0800

    Create LC 40. 最小的 k 个数 快速排序.cpp

---
+++ 22.9/9/LC 40. 最小的 k 个数 快速排序.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> getLeastNumbers(vector<int>& arr, int k) {
+        quickSort(arr, 0, arr.size() - 1);
+        vector<int> res;
+        res.assign(arr.begin(), arr.begin() + k);
+        return res;
+    }
+private:
+    void quickSort(vector<int>& arr, int l, int r) {
+        // 子数组长度为 1 时终止递归
+        if (l >= r) return;
+        // 哨兵划分操作（以 arr[l] 作为基准数）
+        int i = l, j = r;
+        while (i < j) {
+            while (i < j && arr[j] >= arr[l]) j--;
+            while (i < j && arr[i] <= arr[l]) i++;
+            swap(arr[i], arr[j]);
+        }
+        swap(arr[i], arr[l]);
+        // 递归左（右）子数组执行哨兵划分
+        quickSort(arr, l, i - 1);
+        quickSort(arr, i + 1, r);
+    }
+};
+
+
+int main()
+{
+    Solution tool;
+    vector<int>array = { 12,2,3,45,55,6,67 };
+    vector<int>res = tool.getLeastNumbers(array,3);
+    for (int i = 0; i < 3; i++)
+    {
+        cout << res[i] << " ";
+    }
+}
